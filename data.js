859424c5f59e0f8adb58b1b54a488e8662c16eeb
dogierez01 <kalimera54@gmail.com>
2026-02-23 05:25:08 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
+// This is your conversation vault
 const lessonData = {
     "01_Airport_Check-in.wav": {
         english: "Good morning. May I see your passport and ticket, please?",
         dictionary: {
             "Good": "İyi",
-            "morning": "sabahlar",
+            "morning": "günaydın",
             "passport": "pasaport",
             "ticket": "bilet",
             "please": "lütfen",
             "see": "görmek"
         }
     }
-    // We will add the other 20 here later!
 };
