786cff1733ae4ecc5bd29ca0f1164b09c1ef3210
dogierez01 <kalimera54@gmail.com>
2026-02-23 05:17:42 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const lessonData = {
+    "01_Airport_Check-in.wav": {
+        english: "Good morning. May I see your passport and ticket, please?",
+        dictionary: {
+            "Good": "İyi",
+            "morning": "sabahlar",
+            "passport": "pasaport",
+            "ticket": "bilet",
+            "please": "lütfen",
+            "see": "görmek"
+        }
+    }
+    // We will add the other 20 here later!
+};
