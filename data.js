ceb4a4bb9a3bc4f1a47a0502454d7f922ff1115c
dogierez01 <kalimera54@gmail.com>
2026-02-23 05:41:18 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
-// This is your conversation vault
 const lessonData = {
-    "01_Airport_Check-in.wav": {
-        english: "Good morning. May I see your passport and ticket, please?",
-        dictionary: {
-            "Good": "İyi",
-            "morning": "günaydın",
-            "passport": "pasaport",
-            "ticket": "bilet",
-            "please": "lütfen",
-            "see": "görmek"
+    "01_Airport_Check-in.wav": [
+        {
+            speaker: "official",
+            text: "Good morning. May I see your passport and ticket, please?",
+            dictionary: { "Good": "İyi", "morning": "sabahlar", "passport": "pasaport", "ticket": "bilet", "please": "lütfen" }
+        },
+        {
+            speaker: "traveler",
+            text: "Sure, here they are. I have a window seat, right?",
+            dictionary: { "Sure": "Tabii", "here": "işte", "window": "pencere", "seat": "koltuk", "right": "doğru" }
         }
-    }
+    ]
 };
